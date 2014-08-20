a5e6303fd308325502f9a771bd0a80364718d702
Rohan <sarith21@gmail.com>
2014-08-20 15:56:41 -0700

    Adds function to convert string notation to MIDI note number

--- muse/compiler.js
+++ muse/compiler.js
@@ -13,6 +13,23 @@ var endTime = function(expr, time){
   return time;
 };
 
+var noteToMidi = function(note){
+
+  var noteMap = {
+    c: 0,
+    d: 2,
+    e: 4,
+    f: 5,
+    g: 7,
+    a: 9,
+    b: 11
+  };
+
+  var base = 12 + (parseInt(note[1]) * 12);
+  var scale = noteMap[note[0]];
+  return base + scale;
+};
+
 var compile = function(expr){
   var result = [];
 
@@ -25,6 +42,7 @@ var compile = function(expr){
       parseMus(e.left, timer);
       parseMus(e.right, endTime(e.left, timer));
     }else{
+      e.pitch = noteToMidi(e.pitch);
       e.start = timer;
       result.push(e);
     }
@@ -35,3 +53,17 @@ var compile = function(expr){
   return result;
 
 };
+
+var melody_mus =
+    { tag: 'seq',
+      left:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+console.log(melody_mus);
+console.log(compile(melody_mus));
